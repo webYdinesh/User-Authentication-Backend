c546ada6b4f991bf7ead59f4cf4b252920dbf9c9
Dinesh Singh Bisht <121343492+webYdinesh@users.noreply.github.com>
2023-01-01 18:38:44 +0530

    Update UserAuth.js

--- Controllers/UserAuth.js
+++ Controllers/UserAuth.js
@@ -54,7 +54,7 @@ exports.userLogin = async (req, res) => {
     res.cookie(String(existingUser._id), token, {
       expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
     });
     res.status(200).json({
       status: 200,
